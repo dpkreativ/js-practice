ad516e3d26e579fd944ad8667ef518dea67f5e49
Divine <dpkreativ@gmail.com>
2022-01-02 19:37:33 +0100

    create js file, import to html, write problem statement

---
+++ js/2022/January/script001.js
@@ -0,0 +1,16 @@
+/*
+Tobi has a graph showing several expected housing values over the next few years. She has to buy the house in one year and sell it in the next, all while losing money. She wants to keep her financial losses to a minimum. 
+
+Consider the following array, which represents the house price for each year: arr = [10,5,3]. Purchasing in year 2 (at 5) and selling in year 3 results in a minimum loss for her (at 3). In this example, the loss is 5 - 3 = 2.
+
+Complete the function to determine and return the minimum loss given an array of house prices.
+
+
+Sample Input:
+
+10 5 3
+
+Sample Output:
+
+2
+*/
