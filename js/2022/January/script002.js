3dfdada26a8e5bbc4680fd097743da7eef9720ee
Divine <dpkreativ@gmail.com>
2022-01-04 21:13:54 +0100

    set new challenge

---
+++ js/2022/January/script002.js
@@ -0,0 +1,34 @@
+/*
+Valid Parentheses
+
+Given a string s containing just the characters '(", ')', (, '}', 'I' and ']', determine if the input string is valid.
+
+An input string is valid if:
+
+1. Open brackets must be closed by the same type of brackets.
+2. Open brackets must be closed in the correct order.
+
+Constraints:
+1 <= s. length <= 104
+s consists of parentheses only ({[}])
+
+Example 1:
+Input: s="{}[[]]"
+Output: valid
+
+Example 2:
+Input: S = "{[](()[])}"
+Output: valid
+
+Example 3:
+Input: S = "(]"
+Output: invalid
+
+Example 4:
+Input: S = "([))]"
+output: invalid
+
+Example 5:
+Input: S= "{()}"
+Output: valid
+*/
