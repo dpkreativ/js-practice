5eeea1abf1bf63dd1c732127b671f8ede87efbd4
Divine Orji <dpkreativ@gmail.com>
2022-05-29 17:18:28 +0100

    successfully solved diagonalDifference challenge

--- js/2022/May/script003.js
+++ js/2022/May/script003.js
@@ -1,1 +1,28 @@
 // https://www.hackerrank.com/challenges/diagonal-difference/problem?isFullScreen=true
+const matrix = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [9, 8, 9],
+];
+
+function diagonalDifference(arr) {
+  const lastEl = arr.length - 1;
+
+  let leftDiagonal = [],
+    rightDiagonal = [],
+    i;
+
+  for (i = 0; i <= lastEl; i++) {
+    leftDiagonal.push(arr[i][i]);
+    rightDiagonal.push(arr[i][lastEl - i]);
+  }
+
+  const leftSum = leftDiagonal.reduce((a, b) => a + b);
+  const rightSum = rightDiagonal.reduce((a, b) => a + b);
+
+  const difference = Math.abs(leftSum - rightSum);
+
+  return difference;
+}
+
+console.log(diagonalDifference(matrix));
