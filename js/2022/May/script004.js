30f11c8144afc0e09e068c2a89e27ebf810908b9
Divine Orji <dpkreativ@gmail.com>
2022-05-31 11:07:58 +0100

    Solved plusMinus challenge on Hackerrank

--- js/2022/May/script004.js
+++ js/2022/May/script004.js
@@ -1,9 +1,19 @@
 // https://www.hackerrank.com/challenges/plus-minus/problem?isFullScreen=true
 
-const intArray = [];
+const intArray = [-4, 3, -9, 0, 4, 1];
 
 function plusMinus(arr) {
   console.log(arr);
+  let ln = arr.length,
+    val = [0, 0, 0];
+
+  function rounded(x) {
+    return Math.round(x * 1000000) / 1000000;
+  }
+
+  arr.forEach((el) => (el > 0 ? val[0]++ : el < 0 ? val[1]++ : val[2]++));
+
+  val.forEach((el) => console.log(rounded(el / ln)));
 }
 
 plusMinus(intArray);
