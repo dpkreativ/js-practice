82ba89fc6f009c9c58c9bf90c89ea29cfc9c50fd
Divine Orji <dpkreativ@gmail.com>
2022-06-01 22:48:50 +0100

    Write starter code for challenge

--- js/2022/June/script003.js
+++ js/2022/June/script003.js
@@ -1,1 +1,9 @@
 // https://www.hackerrank.com/challenges/time-conversion/problem?isFullScreen=true
+
+const timeString = '';
+
+function timeConversion(s) {
+  console.log(s);
+}
+
+timeConversion(timeString);
