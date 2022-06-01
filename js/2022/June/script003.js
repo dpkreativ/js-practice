1a6b347daba873dedce6ba8964cb4d67b92be50c
Divine Orji <dpkreativ@gmail.com>
2022-06-01 23:28:09 +0100

    Solved timeConversion challenge on Hackerrank

--- js/2022/June/script003.js
+++ js/2022/June/script003.js
@@ -1,9 +1,20 @@
 // https://www.hackerrank.com/challenges/time-conversion/problem?isFullScreen=true
 
-const timeString = '';
+const timeString = '02:05:45PM';
 
 function timeConversion(s) {
   console.log(s);
+  let timeArray = s.split(':');
+  let hour = Number(timeArray[0]),
+    z = timeArray[2];
+
+  z.includes('P') && hour !== 12
+    ? (timeArray[0] = hour + 12)
+    : z.includes('A') && hour === 12
+    ? (timeArray[0] = '00')
+    : null;
+
+  console.log(timeArray.join(':').replace(/[ap]m/i, ''));
 }
 
 timeConversion(timeString);
