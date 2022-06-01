f5217b102746ee1dcecbc62f099ab8c7ae8d2101
Divine Orji <dpkreativ@gmail.com>
2022-06-01 07:26:07 +0100

    Solved minMaxSum challenge

--- js/2022/June/script001.js
+++ js/2022/June/script001.js
@@ -1,9 +1,13 @@
 // https://www.hackerrank.com/challenges/mini-max-sum/problem?isFullScreen=true
 
-const intArray = [];
+const intArray = [1, 3, 4, 5, 6];
 
 function minMaxSum(arr) {
-  console.log(arr);
+  let sortedArr = arr.sort((a, b) => a - b);
+  let min = sortedArr[0] + sortedArr[1] + sortedArr[2] + sortedArr[3];
+  let max = sortedArr[1] + sortedArr[2] + sortedArr[3] + sortedArr[4];
+
+  console.log(`${min} ${max}`);
 }
 
 minMaxSum(intArray);
