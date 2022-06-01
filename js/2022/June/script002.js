0434edc59ba78736166295bd0dd30daa5ba751b2
Divine Orji <dpkreativ@gmail.com>
2022-06-01 07:35:21 +0100

    Write starter function for challenge

--- js/2022/June/script002.js
+++ js/2022/June/script002.js
@@ -1,1 +1,9 @@
 // https://www.hackerrank.com/challenges/birthday-cake-candles/problem?isFullScreen=true
+
+const intArray = [3, 2, 1, 3];
+
+function birthdayCakeCandles(candles) {
+  console.log(candles);
+}
+
+birthdayCakeCandles(intArray);
