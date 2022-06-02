743e5fc4d2d4d94fb3e92222e1171c86651b3b0c
Divine Orji <dpkreativ@gmail.com>
2022-06-02 14:30:53 +0100

    Write the starter function for the challenge

--- js/2022/June/script004.js
+++ js/2022/June/script004.js
@@ -1,1 +1,9 @@
 // https://www.hackerrank.com/challenges/grading/problem?isFullScreen=true
+
+const intArray = [73, 67, 38, 33];
+
+function gradingStudents(grades) {
+  console.log(grades);
+}
+
+gradingStudents(intArray);
