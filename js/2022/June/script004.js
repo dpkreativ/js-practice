dbc616fb4e9e5255fce958519abaad666f4888ec
Divine Orji <dpkreativ@gmail.com>
2022-06-02 13:52:58 +0100

    Write starter files, try to understand challenge

---
+++ js/2022/June/script004.js
@@ -0,0 +1,20 @@
+// https://www.hackerrank.com/challenges/brick-tiling/problem?isFullScreen=true
+
+const input = [
+  ['....', '....'],
+  ['...', '.#.', '...'],
+  ['##', '##'],
+];
+
+function brickTiling(grid) {
+  console.log(grid);
+}
+
+brickTiling(input);
+
+/*
+Note:
+- each grid will have an equal number of blocks in each row and column.
+- This means that omoh I nor sabi wetin I dey do oh.
+- Just found out that the difficulty level of this one is hard.
+*/
