62fde6c30ef5dd08de925751fc9b466dae863b8f
Divine Orji <dpkreativ@gmail.com>
2022-06-04 05:40:05 +0100

    Solved countApplesAndOranges challenge

--- js/2022/June/script005.js
+++ js/2022/June/script005.js
@@ -1,11 +1,23 @@
 // https://www.hackerrank.com/challenges/apple-and-orange/problem?isFullScreen=true
-const sInt = 0,
-  tInt = 0,
-  aInt = 0,
-  bInt = 0,
-  applesArr = [],
-  orangesArr = [];
+const sInt = 7,
+  tInt = 10,
+  aInt = 4,
+  bInt = 12,
+  applesArr = [2, 3, -4],
+  orangesArr = [3, -2, -4];
 
-function countApplesAndOranges(s, t, a, b, apples, oranges) {}
+function countApplesAndOranges(s, t, a, b, apples, oranges) {
+  function range(x) {
+    return x >= s && x <= t;
+  }
+
+  function checkFruits(y, z) {
+    return y.map((el) => range(el + z)).filter((el) => el === true).length;
+  }
+
+  console.log(checkFruits(apples, a));
+
+  console.log(checkFruits(oranges, b));
+}
 
 countApplesAndOranges(sInt, tInt, aInt, bInt, applesArr, orangesArr);
