28d99ff23c2ceab225ff8a6ef14eecd6d430f59e
Divine Orji <dpkreativ@gmail.com>
2022-06-04 05:59:20 +0100

    Create file and starter code for new challenge

---
+++ js/2022/June/script006.js
@@ -0,0 +1,9 @@
+// https://www.hackerrank.com/challenges/kangaroo/problem?isFullScreen=true
+const x1Int = 0,
+  v1Int = 0,
+  x2Int = 0,
+  v2Int = 0;
+
+function kangaroo(x1, v1, x2, v2) {}
+
+kangaroo(x1Int, v1Int, x2Int, v2Int);
