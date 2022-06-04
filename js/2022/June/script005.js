39b3b13146a6338be88e63434a086903141f30ad
Divine Orji <dpkreativ@gmail.com>
2022-06-04 05:14:02 +0100

    Create file and starter code for new challenge

---
+++ js/2022/June/script005.js
@@ -0,0 +1,11 @@
+// https://www.hackerrank.com/challenges/apple-and-orange/problem?isFullScreen=true
+const sInt = 0,
+  tInt = 0,
+  aInt = 0,
+  bInt = 0,
+  applesArr = [],
+  orangesArr = [];
+
+function countApplesAndOranges(s, t, a, b, apples, oranges) {}
+
+countApplesAndOranges(sInt, tInt, aInt, bInt, applesArr, orangesArr);
