43ff354ef38ce8333837e1c452477d0d4f36adcc
Divine Orji <dpkreativ@gmail.com>
2022-06-08 18:58:41 +0100

    Hackerrank kangaroo problem attempt

--- js/2022/June/script006.js
+++ js/2022/June/script006.js
@@ -1,9 +1,24 @@
 // https://www.hackerrank.com/challenges/kangaroo/problem?isFullScreen=true
 const x1Int = 0,
-  v1Int = 0,
-  x2Int = 0,
-  v2Int = 0;
+  v1Int = 3,
+  x2Int = 4,
+  v2Int = 2;
 
-function kangaroo(x1, v1, x2, v2) {}
+function kangaroo(x1, v1, x2, v2) {
+  const ab = Math.abs(x1 - x2),
+    cd = x1 + v1 * ab,
+    ef = x2 + v2 * ab;
 
-kangaroo(x1Int, v1Int, x2Int, v2Int);
+  console.log(cd);
+  console.log(ef);
+
+  return cd === ef ? 'YES' : 'NO';
+
+  // if (ab || cd) {
+  //   console.log('YES');
+  // } else {
+  //   console.log('NO');
+  // }
+}
+
+console.log(kangaroo(x1Int, v1Int, x2Int, v2Int));
