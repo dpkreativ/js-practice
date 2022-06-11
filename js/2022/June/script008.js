17d56faf521c2f59d064c613ea1ceb3f1a6c2718
Divine Orji <dpkreativ@gmail.com>
2022-06-11 20:46:05 +0100

    Set challenge question

---
+++ js/2022/June/script008.js
@@ -0,0 +1,16 @@
+/* QUESTION:
+An ATM machine is loaded with 3 notes: $100, $50, and $20. 
+Write a function that returns an array of numbers, representing the number of each note denomination to be dispensed by the ATM machine when a customer tries to withdraw money.
+
+Example One:
+Amount = $150
+Function return: [1, 1, 0]
+Explanation: This implies that $100 = 1, $50 = 1, $20 = 0
+
+Example Two:
+Amount = $230
+Function return: [1, 1, 4]
+Explanation: This implies that $100 = 1, $50 = 1, $20 = 4
+
+Note that in each case, the function should return based on the minimum number of notes to be dispensed.
+*/
