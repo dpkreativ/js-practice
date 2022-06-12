6c4945edaa712ab056589fbff42dd0bf8e39bbc6
Divine Orji <dpkreativ@gmail.com>
2022-06-12 22:18:03 +0100

    Write starter code for challenge

--- js/2022/June/script007.js
+++ js/2022/June/script007.js
@@ -1,1 +1,8 @@
 // https://www.hackerrank.com/challenges/breaking-best-and-worst-records/problem?isFullScreen=true
+const records = [10, 5, 20, 20, 4, 5, 2, 25, 1];
+
+function breakingRecords(scores) {
+  return scores;
+}
+
+console.log(breakingRecords(records));
