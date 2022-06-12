c25a7c1dcf2ee5ae3b3c9c5481fe8c462d46d29b
Divine Orji <dpkreativ@gmail.com>
2022-06-12 22:33:36 +0100

    First solution iteration for challenge - Passed sample tests

--- js/2022/June/script007.js
+++ js/2022/June/script007.js
@@ -1,8 +1,22 @@
 // https://www.hackerrank.com/challenges/breaking-best-and-worst-records/problem?isFullScreen=true
 const records = [10, 5, 20, 20, 4, 5, 2, 25, 1];
+const records1 = [3, 4, 21, 36, 10, 28, 35, 5, 24, 42];
 
 function breakingRecords(scores) {
-  return scores;
+  let bestWorst = [0, 0],
+    a = scores[0],
+    b = scores[0];
+
+  scores.forEach((el) => {
+    if (el > a) {
+      bestWorst[0]++;
+      a = el;
+    } else if (el < b) {
+      bestWorst[1]++;
+      b = el;
+    }
+  });
+  return bestWorst;
 }
 
 console.log(breakingRecords(records));
