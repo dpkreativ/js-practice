773dcf168191e600b80c39cee615c0afe5c38cf6
Divine Orji <dpkreativ@gmail.com>
2022-06-14 22:19:35 +0100

    Write starter function for new challenge

---
+++ js/2022/June/script009.js
@@ -0,0 +1,10 @@
+// https://www.hackerrank.com/challenges/divisible-sum-pairs/problem?isFullScreen=true
+const intArray = [1, 2, 3, 4, 5, 6],
+  posInt = 5,
+  arrLn = intArray.length;
+
+function divisibleSumPairs(n, k, ar) {
+  return `It's morbin' time!`;
+}
+
+console.log(divisibleSumPairs(arrLn, posInt, intArray));
