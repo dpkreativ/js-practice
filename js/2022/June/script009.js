708009aabc5f605f90b9d32477b6e856487dd48b
Divine Orji <dpkreativ@gmail.com>
2022-06-14 22:49:25 +0100

    Solved divisible sum pairs challenge

--- js/2022/June/script009.js
+++ js/2022/June/script009.js
@@ -1,10 +1,19 @@
 // https://www.hackerrank.com/challenges/divisible-sum-pairs/problem?isFullScreen=true
-const intArray = [1, 2, 3, 4, 5, 6],
-  posInt = 5,
+const intArray = [1, 3, 2, 6, 1, 2],
+  posInt = 3,
   arrLn = intArray.length;
 
 function divisibleSumPairs(n, k, ar) {
-  return `It's morbin' time!`;
+  let pairs = 0,
+    i,
+    filtered;
+
+  for (i = 0; i < n; i++) {
+    filtered = ar.filter((el, idx) => i !== idx);
+    filtered.forEach((el) => ((ar[i] + el) % k === 0 ? pairs++ : null));
+  }
+
+  return pairs / 2;
 }
 
 console.log(divisibleSumPairs(arrLn, posInt, intArray));
