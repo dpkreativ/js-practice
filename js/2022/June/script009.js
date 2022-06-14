a00aeabf7614c2cf8a6be75ee43e736551d8a2f5
Divine Orji <dpkreativ@gmail.com>
2022-06-14 23:00:32 +0100

    Update solution with optimized code

--- js/2022/June/script009.js
+++ js/2022/June/script009.js
@@ -3,17 +3,31 @@ const intArray = [1, 3, 2, 6, 1, 2],
   posInt = 3,
   arrLn = intArray.length;
 
+// function divisibleSumPairs(n, k, ar) {
+//   let pairs = 0,
+//     i,
+//     filtered;
+
+//   for (i = 0; i < n; i++) {
+//     filtered = ar.filter((el, idx) => i !== idx);
+//     filtered.forEach((el) => ((ar[i] + el) % k === 0 ? pairs++ : null));
+//   }
+
+//   return pairs / 2;
+// }
+
 function divisibleSumPairs(n, k, ar) {
   let pairs = 0,
     i,
-    filtered;
+    j;
 
   for (i = 0; i < n; i++) {
-    filtered = ar.filter((el, idx) => i !== idx);
-    filtered.forEach((el) => ((ar[i] + el) % k === 0 ? pairs++ : null));
+    for (j = i + 1; j < n; j++) {
+      (ar[i] + ar[j]) % k === 0 ? pairs++ : null;
+    }
   }
 
-  return pairs / 2;
+  return pairs;
 }
 
 console.log(divisibleSumPairs(arrLn, posInt, intArray));
