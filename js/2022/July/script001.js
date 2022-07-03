d6596597dd5562e87d11db7ded93468bd51422f1
Divine Orji <dpkreativ@gmail.com>
2022-07-03 19:35:56 +0100

    feat: 🎸 learn about JavaScript Maps
    In this codebase, I am learning and practicing how to use JavaScript Map

---
+++ js/2022/July/script001.js
@@ -0,0 +1,19 @@
+// Learning about Maps in JavaScript
+
+const user = new Map([
+  ['First Name', 'Divine'],
+  ['Last Name', 'Orji'],
+  ['Age', 24],
+  [true, 'Cheese'],
+]);
+
+console.log(user.size);
+
+/*
+Map(4) {
+  'First Name' => 'Divine',       
+  'Last Name' => 'Orji',
+  'Age' => 24,
+  true => 'Cheese'
+}
+*/
