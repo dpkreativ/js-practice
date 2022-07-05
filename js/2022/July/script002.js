31f4884ba08feba450ae814782d8fb023caf347d
Divine Orji <dpkreativ@gmail.com>
2022-07-05 10:50:30 +0100

    Solve Sieve of Eratostenes algorithm

--- js/2022/July/script002.js
+++ js/2022/July/script002.js
@@ -1,7 +1,34 @@
 // Sieve of Eratosthenes Algorithm
 
-function getPrimes(min, max) {
-  return max;
+function getPrimes(input) {
+  // Create an array where each element starts as true
+  const numsArr = Array.from({ length: input + 1 }, () => true);
+
+  // Create an array to store the prime numbers
+  const primeNumbers = [];
+
+  /*
+  Loop through numsArr starting from numsArr[2]
+  because 0 and 1 are definitely not prime numbers
+  */
+  for (let i = 2; i <= input; i++) {
+    // Check if numsArr[i] === true
+    if (numsArr[i]) {
+      // add the i to the primeNumbers array
+      primeNumbers.push(i);
+
+      /* 
+      convert all elements in the numsArr 
+      whose indexes are multiples of i 
+      to false
+      */
+      for (let j = i + i; j <= input; j += i) {
+        numsArr[j] = false;
+      }
+    }
+  }
+
+  return primeNumbers;
 }
 
-console.log(getPrimes(0, 100));
+console.log(getPrimes(21));
