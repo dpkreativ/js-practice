dfd31908dbbd2c5bb2cf3823abe0fc50b3fbcddf
Divine Orji <dpkreativ@gmail.com>
2022-07-05 15:13:33 +0100

    Optimize algorithm

--- js/2022/July/script002.js
+++ js/2022/July/script002.js
@@ -4,19 +4,11 @@ function getPrimes(input) {
   // Create an array where each element starts as true
   const numsArr = Array.from({ length: input + 1 }, () => true);
 
-  // Create an array to store the prime numbers
-  const primeNumbers = [];
-
-  /*
-  Loop through numsArr starting from numsArr[2]
-  because 0 and 1 are definitely not prime numbers
-  */
-  for (let i = 2; i <= input; i++) {
+  // Loop through numsArr starting from numsArr[2]
+  // keep running the loop until input is less than i^2 (i squared)
+  for (let i = 2; i * i <= input; i++) {
     // Check if numsArr[i] === true
     if (numsArr[i]) {
-      // add the i to the primeNumbers array
-      primeNumbers.push(i);
-
       /* 
       convert all elements in the numsArr 
       whose indexes are multiples of i 
@@ -28,6 +20,20 @@ function getPrimes(input) {
     }
   }
 
+  /*
+  Using Array.prototype.reduce() method:
+    loop through each element in numsArr[]
+      if element === true, 
+      add the index of that element to result[]
+      return result
+  */
+  const primeNumbers = numsArr.reduce(
+    (result, element, index) =>
+      element ? (result.push(index), result) : result,
+    []
+  );
+
+  // Return primeNumbers[]
   return primeNumbers;
 }
 
