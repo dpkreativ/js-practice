c6ca08697c4e0c2bbe4a04bc2f47f0045bbcfb2d
Divine Orji <dpkreativ@gmail.com>
2022-07-07 07:26:46 +0100

    finish examples 2 and 3 for the splice() method

--- js/2022/July/script004.js
+++ js/2022/July/script004.js
@@ -4,9 +4,16 @@
 // INITIAL ARRAY
 const gadgets = ['phone', 'laptop', 'headphones', 'earbuds'];
 
-// Example 1
+// ==== Example 1 ====
+/*
+Starting from index 3,
+-> remove 0 elements
+-> add 'solar lamp', 'clock', 'watch'
+*/
 gadgets.splice(3, 0, 'solar lamp', 'clock', 'watch');
-console.log(gadgets);
+
+// -> print to console
+// console.log(gadgets);
 
 /*
 [
@@ -18,4 +25,41 @@ console.log(gadgets);
   'watch',     
   'earbuds'    
 ]
+*/
+
+// ==== Example 2 ====
+/*
+Starting from index 5 in our updated gadgets[] array,
+-> remove 1 element
+-> add 'smart watch'
+*/
+gadgets.splice(5, 1, 'smart watch');
+
+// -> print to console
+// console.log(gadgets);
+
+/*
+[
+  'phone',
+  'laptop',
+  'headphones',
+  'solar lamp',
+  'clock',
+  'smart watch',
+  'earbuds'
+]
+*/
+
+// ==== Example 3 ====
+/*
+Starting from index 3 in our updated gadgets[] array,
+-> remove 2 elements
+*/
+gadgets.splice(3, 2);
+
+// -> print to console
+// console.log(gadgets);
+
+/*
+[ 'phone', 'laptop', 'headphones', 'smart watch', 'earbuds' ] 
 */
