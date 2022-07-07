38fba49a9bf5d282c295fcaec48bad172acf2274
Divine Orji <dpkreativ@gmail.com>
2022-07-07 05:43:06 +0100

    code examples of Array.splice()

---
+++ js/2022/July/script004.js
@@ -0,0 +1,8 @@
+// Array splice vs slice examples
+
+// ==== Array.prototype.splice() ====
+// INITIAL ARRAY
+const gadgets = ['phone', 'laptop', 'headphones', 'earbuds'];
+
+gadgets.splice(3, 0, 'solar lamp', 'clock', 'watch');
+console.log(gadgets);
