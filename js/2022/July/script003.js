699514c1aa7bf32979cfc7d20c03bf2c59b11933
Divine Orji <dpkreativ@gmail.com>
2022-07-07 05:42:35 +0100

    test Set methods

--- js/2022/July/script003.js
+++ js/2022/July/script003.js
@@ -1,1 +1,12 @@
 // Learn about JavaScript Set
+
+const trySet = new Set();
+
+trySet.add('Hello');
+trySet.add('Bye');
+
+let school = { school: 'MECDA' };
+trySet.add(school);
+trySet.delete(school);
+
+console.log(trySet.entries());
