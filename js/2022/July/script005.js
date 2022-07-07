b25eff96303a8ef676b8ec9b97f6694f7d0cfb88
Divine Orji <dpkreativ@gmail.com>
2022-07-07 09:55:57 +0100

    code example 2 for Array.slice()

--- js/2022/July/script005.js
+++ js/2022/July/script005.js
@@ -14,11 +14,24 @@ const grocery = [
 -> copy all elements from index 2 to index 5 
     except the element at index 5
 */
-let protein = grocery.slice(2, 5);
+const protein = grocery.slice(2, 5);
 
 // -> print to console
 console.log(protein);
 
 /*
 [ 'eggs', 'meat', 'fish' ]
+*/
+
+// ==== Example 2 ====
+/*
+-> copy all elements from index 1 to the end of the array
+*/
+const noTomatoes = grocery.slice(1);
+
+// -> print to console
+console.log(noTomatoes);
+
+/*
+[ 'pumpkin leaves', 'eggs', 'meat', 'fish', 'onions' ]
 */
