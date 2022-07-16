ec001c74657c1abd6b38d7bb35d13f640d2b28c6
Divine Orji <dpkreativ@gmail.com>
2022-07-16 21:51:10 +0100

    testing event listeners

--- js/2022/July/script006.js
+++ js/2022/July/script006.js
@@ -2,3 +2,9 @@
  * Tonight's Challenge:
  * Save form data to localHost using JavaScript and HTML
  */
+
+let getForm = document.getElementById('test-form');
+
+getForm.addEventListener('submit', (e) => {
+  console.log(e.target.value);
+});
