99f7d3b8ed73229ebf287eb0ddd7f7a5f9929714
Divine Orji <dpkreativ@gmail.com>
2022-07-31 18:59:23 +0100

    Trying out a C challenge in JS

---
+++ js/2022/July/script010.js
@@ -0,0 +1,10 @@
+/**
+ * Find the the index of ! in a string
+ * It prints => "Exclamation mark is found in index number <! index>"
+ */
+
+function shoutIndex(x) {
+  console.log(`Exclamation mark is found in index number ${x.indexOf('!')}`);
+}
+
+shoutIndex('Wow. This is nice!');
