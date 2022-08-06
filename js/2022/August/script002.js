dfa655e46327760de38804ad9e16feab6967d815
Divine Orji <dpkreativ@gmail.com>
2022-08-06 06:14:14 +0100

    Match string despite its position

--- js/2022/August/script002.js
+++ js/2022/August/script002.js
@@ -1,9 +1,11 @@
 // Learning Regex with Arit
 
-const regex1 = /[Dd]ivine/;
+const regex1 = /Divine/;
 const string1 = 'vi';
 const string2 = 'Divine';
 const string3 = 'divine';
 const string4 = 'Rivine';
+const string5 = 'DivineOrji';
+const string6 = 'OrjiDivine';
 
-console.log(regex1.test(string4));
+console.log(regex1.test(string6));
