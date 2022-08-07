67eb7fb4563d1121c8bbe6d5a9eee4fe22f5480b
Divine Orji <dpkreativ@gmail.com>
2022-08-07 08:09:16 +0100

    Write regex to validate Nigerian numbers

--- js/2022/August/script002.js
+++ js/2022/August/script002.js
@@ -8,4 +8,15 @@ const string4 = 'Rivine';
 const string5 = 'DivineOrji';
 const string6 = 'OrjiDivine';
 
-console.log(regex1.test(string4));
+// console.log(regex1.test(string4));
+
+// Phone number validation
+
+const validNumberPattern = /^(0|\+234)\d{10}$/;
+
+const n1 = '+2349021824073';
+const n2 = '789210';
+const n3 = '+7128da';
+const n4 = '09021824073';
+
+console.log(validNumberPattern.test(n4));
