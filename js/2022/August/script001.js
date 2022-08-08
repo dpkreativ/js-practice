d1c367b8dbafc524e794ee24b7b779f77355cd31
Divine Orji <dpkreativ@gmail.com>
2022-08-08 18:51:30 +0100

    Ensure that the length of the string >= n

--- js/2022/August/script001.js
+++ js/2022/August/script001.js
@@ -4,4 +4,10 @@
 
 function repeatedString(s, n) {
   // write your code here
+  // Step one: Ensure that the length of the string is greater than n
+  while (s.length < n) {
+    s = s + s;
+  }
+
+  return s;
 }
