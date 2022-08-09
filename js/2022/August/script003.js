40e1f94cad4f7abb6fcc0a90a6e7fe90edba96de
Divine Orji <dpkreativ@gmail.com>
2022-08-09 21:14:09 +0100

    Trying Data Structures Challenge

---
+++ js/2022/August/script003.js
@@ -0,0 +1,34 @@
+// Data Structures and Algorithm Study Group with Ayomide Oyekanmi
+/**
+ * Given an arr[] of size N, containing elements from 1 to N
+ * Find the position after K jumps from index 1, where the jump from the ith index
+ * sends to arr[i]
+ */
+
+/**
+ * Explanation:
+ * Assuming you have an array like:
+ *
+ * size = 6
+ * arr = [1, 2, 3, 4, 5, 6]
+ *
+ * If you're making three jumps:
+ * K = 3
+ *
+ * First Jump:
+ * START FROM arr[0] which has a value of 1
+ * LAND IN arr[1] which has a value of 2
+ *
+ * Second Jump:
+ * START FROM arr[2] which has a value of 3
+ * LAND IN arr[4] which has a value of 5
+ *
+ * START FROM arr[5] which has a value of 6
+ *
+ * Omoh na to Google the correct challenge oh.
+ *
+ * Anyway to solve this:
+ *
+ * while / for => K times
+ *
+ */
